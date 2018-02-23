c1736d061ccaafadbe6686e3ffbc14b3652cbbef
Sergey Sivak <sergeysivak@macbook.local>
2018-02-24 00:04:13 +0200

    fixed removing .xctemplate during replacement & changed template filename

--- install.swift
+++ install.swift
@@ -8,7 +8,7 @@
 
 import Foundation
 
-let templateName = "Module VIPER.xctemplate"
+let templateName = "Viper.xctemplate"
 let destinationRelativePath = "/Platforms/iPhoneOS.platform/Developer/Library/Xcode/Templates/Project Templates/iOS/Application"
 
 func printInConsole(_ message:Any){
@@ -29,8 +29,8 @@ func moveTemplate(){
             printInConsole("✅  Template installed succesfully 🎉. Enjoy it 🙂")
             
         }else{
-            
-            try _ = fileManager.replaceItemAt(URL(fileURLWithPath:"\(destinationPath)/\(templateName)"), withItemAt: URL(fileURLWithPath:templateName))
+            try fileManager.removeItem(atPath: "\(destinationPath)/\(templateName)")
+            try fileManager.copyItem(atPath: templateName, toPath: "\(destinationPath)/\(templateName)")
             
             printInConsole("✅  Template already exists. So has been replaced succesfully 🎉. Enjoy it 🙂")
         }
@@ -52,7 +52,7 @@ func shell(launchPath: String, arguments: [String]) -> String
     
     let data = pipe.fileHandleForReading.readDataToEndOfFile()
     let output = String(data: data, encoding: String.Encoding.utf8)!
-    if output.characters.count > 0 {
+    if output.count > 0 {
         //remove newline character.
         let lastIndex = output.index(before: output.endIndex)
         return String(output[output.startIndex ..< lastIndex])
